d0773f4762731039d1e686e73fcbe392deef87d8
Brenda Pereira Camara <brendacamara@outlook.com>
2022-05-30 13:24:59 -0300

    Add files via upload

--- Cadastro.java
+++ Cadastro.java
@@ -4,17 +4,14 @@ import java.util.stream.Stream;
 public class Cadastro
 {
     Funcionario[] bd = {new Funcionario("Maria", "100.111.111-99", 20, 40, false), new Funcionario("João", "200.111.111-99", 10, 40, false), new Funcionario("Pedro", "300.111.111-99", 15, 40, false), new Funcionario("Bruno", "400.111.111-99", 30, 44, true), new Funcionario("Michael", "500.111.111-99", 10, 40, false)};
-         
-     /*
- Imprimir percentual de funcionário que possuem filhos;   
-    */
+
      public static int findIndex(Funcionario[] arr, String cpfProcurado)
     {
        int len = arr.length;
         int i = 0;
  
         while (i < len) {
-            if (arr[i].cpf == cpfProcurado) {
+            if (arr[i].cpf.equals(cpfProcurado)) {
                 return i;
             }
             else {
@@ -24,6 +21,16 @@ public class Cadastro
         return -1;
     }        
 
+    public Funcionario[] InserirUsuario(String nome, String cpf, double valorHora, double cargaHoraria, Boolean filhos){
+        Funcionario newFuncionario = new Funcionario(nome, cpf, valorHora, cargaHoraria, filhos);
+        
+        Funcionario[] array = Arrays.copyOf(bd, bd.length + 1);
+        array[array.length - 1] = newFuncionario;
+        bd = array;
+        System.out.println("Funcionário " + nome + " adicionado. ");
+        return bd;
+    }
+    
     public void ListarUsuarios(){
         for (int i = 0; i < bd.length; i++) {
             Funcionario current = bd[i];
@@ -34,22 +41,24 @@ public class Cadastro
     public void removerUsuario(String cpfProcurado){
         int index = findIndex(bd, cpfProcurado);
         if(index >= 0){
-            System.out.print("Usuário "+ bd[index].nome +" removido");
+            System.out.print("Usuário "+ bd[index].nome +" removido \n");
             for (int i = index; i < bd.length - 1; i++) {
                 bd[i] = bd[i + 1];
             }
             bd = Arrays.copyOf(bd, bd.length - 1);
         }else{
-            System.out.print("Nenhum funcionário encontrado com este cpf.");
+            System.out.print("Nenhum funcionário encontrado com este cpf.\n");
         }
         
     }
     
    public void EditarSalario (String cpfProcurado, double novoSalario) {
         int index = findIndex(bd, cpfProcurado);
-        bd[index].setSalario(novoSalario);
-        System.out.print(bd[index]);
-   }
+        if(index > 0){
+            bd[index].setSalario(novoSalario);
+            System.out.print("Salário de: " + bd[index].nome + " alterado. \n");
+        }
+    }
    
     public static int findIndexOfMaiorSalario(Funcionario[] arr)
     {
--- Menu.java
+++ Menu.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * Escreva uma descrição da classe Menu aqui.
@@ -7,27 +8,67 @@
  */
 public class Menu
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * Construtor para objetos da classe Menu
-     */
-    public Menu()
-    {
-        // inicializa variáveis de instância
-        x = 0;
+    
+    public static void main(String args[]){
+        Boolean programaRodando = true;
+        Cadastro cadastro = new Cadastro();
+        
+        while(programaRodando){        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escolha uma opção. \n 1.Cadastrar novo funcionário \n 2.Listar todos os funcionários \n 3. Remover um funcionário pelo CPF \n 4.Editar o salário de um funcionário pelo CPF \n 5.Imprimir funcionário com maior salário \n 6. Imprimir percentual de funcionários que tem filhos \n 7. Sair do programa");
+        int opcao = sc.nextInt();
+        if(opcao == 1){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o nome do funcionário: ");
+            String nome = scanner.nextLine();
+            System.out.println("Digite o CPF do funcionário: ");
+            String cpf = scanner.nextLine();
+            System.out.println("Digite o valor/hora do funcionário: ");
+            int valorHora = scanner.nextInt();
+            System.out.println("Digite a carga horária do funcionário: ");
+            int cargaHoraria = scanner.nextInt(); 
+            System.out.println("Digite true se o funcionário tem filhos e false se não: ");
+            Boolean filhos = scanner.nextBoolean();
+            Funcionario[] result = cadastro.InserirUsuario(nome, cpf, valorHora, cargaHoraria, filhos);
+             for (int i = 0; i < result.length; i++) {
+                Funcionario current = result[i];
+                System.out.println("- Nome: " + current.nome + ", cpf: " + current.cpf + ", valor hora: " + current.valorHora + ", carga horária: " + current.cargaHoraria + ", tem filhos: " + current.filhos);
+            }
+        }
+        
+        if(opcao == 2){
+            cadastro.ListarUsuarios();
+        }
+        
+        if(opcao == 3){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o cpf do funcionário: ");
+            String cpf = scanner.nextLine();
+            System.out.print(cpf);
+            System.out.print(cpf.toString());
+            cadastro.removerUsuario(cpf);
+        }
+        
+        if(opcao == 4){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o cpf do funcionário: ");
+            String cpf = scanner.nextLine();
+            System.out.println("Digite o novo salário funcionário: ");
+            double novoSalario = scanner.nextDouble();
+            cadastro.EditarSalario(cpf, novoSalario);
+        }
+        
+        if(opcao == 5){
+            cadastro.getMaiorSalario();
+        }
+        
+        if(opcao == 6){
+            cadastro.percentualComFilhos();
+        }
+        
+        if(opcao == 7){
+            programaRodando = false;
+        }
     }
-
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
     }
 }
